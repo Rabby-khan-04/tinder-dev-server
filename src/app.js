f4dd0733c391f966ac89a7c553ac4881899154f5
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-11-01 20:03:03 +0600

    Update dotenv config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
